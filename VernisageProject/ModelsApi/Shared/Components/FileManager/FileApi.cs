b780157beed3bc707a056abf6f3efd52deaede19
NekIP <Shestakov.rm@list.ru>
2017-11-06 20:15:50 +0300

    Added api models

---
+++ VernisageProject/ModelsApi/Shared/Components/FileManager/FileApi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using VernisageProject.Models.Shared.Components.FileManager;
+
+namespace VernisageProject.ModelsApi.Shared.Components.FileManager {
+	public class FileApi {
+		public string Name { get; set; }
+		public int Length { get; set; }
+		public DateTime DateCreated { get; set; }
+		public string Type { get; set; }
+
+		public static FileApi Map(File item) {
+			return new FileApi {
+				Name = item.Name,
+				Length = item.Length,
+				DateCreated = item.DateCreated,
+				Type = item.Type.ToString()
+			};
+		}
+	}
+}
