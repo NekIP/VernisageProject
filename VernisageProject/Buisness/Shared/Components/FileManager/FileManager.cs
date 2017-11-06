412c9278d3e7e4e2d61aa42675e8d9bffcfd2439
NekIP <Shestakov.rm@list.ru>
2017-11-06 20:26:23 +0300

    Change logic

--- VernisageProject/Buisness/Shared/Components/FileManager/FileManager.cs
+++ VernisageProject/Buisness/Shared/Components/FileManager/FileManager.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace VernisageProject.Buisness.Shared.Components.FileManager {
 	public interface IFileManager {
+		Task Load(IFormFileCollection files);
 	}
 
 	public class FileManager : IFileManager {
+		private readonly IHostingEnvironment AppEnvironment;
+		public FileManager(IHostingEnvironment appEnvironment) {
+			AppEnvironment = appEnvironment;
+		}
 
+		public async Task Load(IFormFileCollection files) {
+			foreach (var file in files) {
+				var filename = ContentDispositionHeaderValue
+								.Parse(file.ContentDisposition)
+								.FileName
+								.Trim('"');
+				var path = Path.Combine(AppEnvironment.ContentRootPath, "wwwroot/hosting", filename);
+				using (var fileStream = new FileStream(path, FileMode.Create)) {
+					using (var fileReading = file.OpenReadStream()) {
+						var bytes = new byte[fileReading.Length];
+						await fileReading.ReadAsync(bytes, 0, bytes.Length);
+						await fileStream.WriteAsync(bytes, 0, bytes.Length);
+					}
+				}
+			}
+		}
 	}
 }
--- VernisageProject/Controllers/FileManagerController.cs
+++ VernisageProject/Controllers/FileManagerController.cs
@@ -6,34 +6,21 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using VernisageProject.Buisness.Shared.Components.FileManager;
 
 namespace VernisageProject.Controllers {
 	[Route("file-manager")]
 	public class FileManagerController : Controller {
-
-		private readonly IHostingEnvironment AppEnvironment;
-		public FileManagerController(IHostingEnvironment appEnvironment) {
-			AppEnvironment = appEnvironment;
+		private readonly IFileManager FileManager;
+		public FileManagerController(IFileManager fileManager) {
+			FileManager = fileManager;
 		}
 
 		[HttpPost]
 		[Route("load")]
 		public async Task Load() {
 			var files = Request.Form.Files;
-			foreach (var file in files) {
-				var filename = ContentDispositionHeaderValue
-								.Parse(file.ContentDisposition)
-								.FileName
-								.Trim('"');
-				var path = Path.Combine(AppEnvironment.ContentRootPath, "wwwroot/hosting", filename);
-				using (var fileStream = new FileStream(path, FileMode.Create)) {
-					using (var fileReading = file.OpenReadStream()) {
-						var bytes = new byte[fileReading.Length];
-						await fileReading.ReadAsync(bytes, 0, bytes.Length);
-						await fileStream.WriteAsync(bytes, 0, bytes.Length);
-					}
-				}
-			}
+			await FileManager.Load(files);
 		}
 	}
 }
