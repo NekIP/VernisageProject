e47e2565a0a8da1e0beab4873a1bbc5961d8bcdc
NekIP <Shestakov.rm@list.ru>
2017-11-06 20:08:46 +0300

    Small fix

--- VernisageProject/Models/Shared/Components/FileManager/File.cs
+++ VernisageProject/Models/Shared/Components/FileManager/File.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VernisageProject.Models.Shared.Components
-{
-    public class File
-    {
-    }
+namespace VernisageProject.Models.Shared.Components.FileManager {
+	public class File {
+	}
 }
