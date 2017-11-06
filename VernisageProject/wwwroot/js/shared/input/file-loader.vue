d6d8bb4235e6061e73f796f72718d277caae742b
NekIP <Shestakov.rm@list.ru>
2017-11-06 15:54:11 +0300

    Added file-loader on a page

--- VernisageProject/wwwroot/js/shared/input/file-loader.vue
+++ VernisageProject/wwwroot/js/shared/input/file-loader.vue
@@ -1,5 +1,19 @@
 ﻿<template>
+	<input type="file" multiple="multiple">
 </template>
 
 <script>
+	export default {
+		props: [],
+		data: function () {
+			return {
+			}
+		},
+		computed: {
+
+		},
+		methods: {
+
+		}
+	}
 </script>
