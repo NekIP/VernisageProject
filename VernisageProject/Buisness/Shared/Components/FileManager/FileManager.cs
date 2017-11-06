f3e29e28a642b48218f00f07d404eddb6b4c676e
NekIP <Shestakov.rm@list.ru>
2017-11-06 20:19:10 +0300

    Added file manager buisnes

---
+++ VernisageProject/Buisness/Shared/Components/FileManager/FileManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VernisageProject.Buisness.Shared.Components.FileManager {
+	public interface IFileManager {
+
+	}
+
+	public class FileManager {
+	}
+}
