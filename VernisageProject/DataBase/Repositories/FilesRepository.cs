eca0ecbcb00d61c100d40147b841610112dbc314
Шестаков Никита <Shestakov.rm@list.ru>
2017-11-08 19:10:25 +0300

    Added file manager repository

--- VernisageProject/DataBase/Repositories/FilesRepository.cs
+++ VernisageProject/DataBase/Repositories/FilesRepository.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using VernisageProject.Models.Shared.Components.FileManager;
 
 namespace VernisageProject.DataBase.Repositories
 {
-    public class FilesRepository
-    {
-    }
+	public interface IFilesRepository : IRepository<File> { }
+
+    public class FilesRepository : Repository<File>, IFilesRepository {
+		public FilesRepository(IConfiguration configuration) : base("fileManager", configuration) { }
+	}
 }
