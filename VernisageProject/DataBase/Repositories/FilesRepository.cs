80d96e665a4208e70b927d333012b91a32cc7c33
Шестаков Никита <Shestakov.rm@list.ru>
2017-11-08 19:48:56 +0300

    Added repository methods

--- VernisageProject/DataBase/Repositories/FilesRepository.cs
+++ VernisageProject/DataBase/Repositories/FilesRepository.cs
@@ -16,11 +16,37 @@ namespace VernisageProject.DataBase.Repositories {
 			await this.Remove(x => x.PhysicalPath == filePath);
 		}
 
-		public async Task CopyFile(string filePath) {
-			var file = (await this.Where(x => x.PhysicalPath == filePath).Execute())
+		public async Task CopyFile(string physicalFilePath, string newFileName) {		
+			try {
+				var file = await GetFileFromPhysicalPath(physicalFilePath);
+				file.PhysicalPath = file.PhysicalPath.Replace(file.Name, newFileName);
+				file.Path = file.Path.Replace(file.Name, newFileName);
+				file.HRef = file.HRef.Replace(file.Name, newFileName);
+				await this.Add(file);
+			}
+			catch (Exception e) {
+				throw e;
+			}
+		}
+
+		public async Task MoveFile(string physicalFilePath, string newPhysicalFilePath, string newFilePath) {
+			try {
+				var file = await GetFileFromPhysicalPath(physicalFilePath);
+				file.PhysicalPath = newPhysicalFilePath;
+				file.Path = newFilePath;
+				file.HRef = "-"; // TODO
+				await this.Update(x => x.PhysicalPath == physicalFilePath, file);
+			}
+			catch (Exception e) {
+				throw e;
+			}
+		}
+
+		public async Task<UserFile> GetFileFromPhysicalPath(string physicalFilePath) {
+			var result = (await this.Where(x => x.PhysicalPath == physicalFilePath).Execute())
 				.FirstOrDefault();
-			if (file != null) {
-				
+			if (result != null) {
+				return result;
 			}
 			else {
 				throw new FileNotFoundException();
