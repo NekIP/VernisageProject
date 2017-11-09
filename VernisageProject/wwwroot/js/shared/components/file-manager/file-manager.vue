277433bc39622bb36a7547fb74a734e1ee908251
Шестаков Никита <Shestakov.rm@list.ru>
2017-11-09 23:34:55 +0300

    Added scss and teplate in file manager

--- VernisageProject/wwwroot/js/shared/components/file-manager/file-manager.vue
+++ VernisageProject/wwwroot/js/shared/components/file-manager/file-manager.vue
@@ -1,1 +1,17 @@
-﻿
+﻿<template>
+	<div class="file-manager">
+
+	</div>
+</template>
+
+<script>
+	export default {
+		props: [],
+		data: function () {
+			return {
+				currentPath: "/",
+				files: []
+			}
+		},
+	}
+</script>
